9733004ab129222122442192963c5ae92f9b4367
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-03-17 22:22:27 +0530

    Made minor changes

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-# posts = [
-#     {
-#         'author':'Collen Hoover',
-#         'title': 'Ugly Love',
-#         'summary':'lorem ipsum',
-#         'date_posted': 'January 16th, 2023',
-#     },
-#     {
-#         'author':'Collen Hoover',
-#         'title': 'Verity',
-#         'summary':'lorem ipsum',
-#         'date_posted': 'January 16th, 2023',
-#     },
-#     {
-#         'author':'Collen Hoover',
-#         'title': 'It Ends With Us',
-#         'summary':'lorem ipsum',
-#         'date_posted': 'January 16th, 2023',
-#     }
-# ]
-
-
 def home(request):
     return render(request,'blog/home.html',{
         'posts':Post.objects.all()
