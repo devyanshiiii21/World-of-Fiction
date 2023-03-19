3befc5c38fd59ec771f123e4725cac4507558ace
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-03-19 23:35:00 +0530

    Worked with List view and Detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='Blog-home'),
-    path('about/',views.about, name='Blog-About')
+    path('', PostListView.as_view(), name='Blog-home'),
+    path('about/',views.about, name='Blog-About'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 def home(request):
     return render(request,'blog/home.html',{
         'posts':Post.objects.all()
     })
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request,'blog/about.html')
