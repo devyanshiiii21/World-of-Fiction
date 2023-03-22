d059b4ac199ce51eea03ead4997c89a3bb07e5a5
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-03-22 20:36:26 +0530

    Worked with the Create View

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView
+) 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='Blog-home'),
     path('about/',views.about, name='Blog-About'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail')
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new', PostCreateView.as_view(), name='post-create')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 def home(request):
     return render(request,'blog/home.html',{
         'posts':Post.objects.all()
@@ -16,6 +19,10 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'summary']
 
 def about(request):
     return render(request,'blog/about.html')
